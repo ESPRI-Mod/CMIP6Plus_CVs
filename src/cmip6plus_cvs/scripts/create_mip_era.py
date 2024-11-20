0838fb27ceecb3a28a5c93450bda483205440c65
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2024-11-20 10:59:23 +0100

    mip_era

---
+++ src/cmip6plus_cvs/scripts/create_mip_era.py
@@ -0,0 +1,28 @@
+import os, json
+
+save_dir = "mip_era"
+os.makedirs(save_dir, exist_ok=True)
+
+
+context = {
+     
+      "@context": {
+      "@base":"https://espri-mod.github.io/mip-cmor-tables/mip_era/"}
+    }
+file_path = os.path.join(save_dir, "000_context.jsonld")
+
+with open(file_path, 'w') as f:
+    json.dump(context, f, indent=4)
+
+
+data = {
+    "@context": "000_context.jsonld",
+    "@id": "cmip6plus"
+}
+
+file_path = os.path.join(save_dir, "ripf.json")
+
+with open(file_path, 'w') as f:
+    json.dump(data, f, indent=4)
+
+
